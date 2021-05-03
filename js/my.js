b026aefb5fc9f4dcbc1bae35ea63d6cee58e2798
NikDimer <60222621+NikDimer@users.noreply.github.com>
2021-05-03 15:11:47 +0300

    Update my.js

--- js/my.js
+++ js/my.js
@@ -40,6 +40,39 @@ var material = [
 var cube2 = new THREE.Mesh( geometry, material );
 scene.add( cube2 );
 cube2.position.x += 40;
+
+var geometry = new THREE.SphereGeometry(3, 4, 4);
+var material = new THREE.MeshNormalMaterial();
+var sphere = new THREE.Mesh( geometry, material );
+sphere.position.z -= 30;
+scene.add( sphere );
+
+var light = new THREE.DirectionalLight( 0xffffff );
+light.position.z += 5;
+light.position.y += 1;
+light.intensity = 3;
+console.log(light)
+scene.add(light);
+
+var geometry = new THREE.SphereGeometry(6, 32, 32);
+var material = new THREE.MeshLambertMaterial({color: 0x0000ff, transparent: true, opacity: 0.3});
+var sphere1 = new THREE.Mesh( geometry, material );
+sphere1.position.z -= 30;
+scene.add( sphere1 );
+
+var geometry = new THREE.BoxGeometry( 12, 12, 12 );
+var edges = new THREE.EdgesGeometry( geometry );
+var line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+line.position.z -= 30;
+console.log(line);
+scene.add( line );
+
+var geometry = new THREE.OctahedronGeometry(13, 1);
+var edges = new THREE.EdgesGeometry( geometry );
+var line1 = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+line1.position.z -= 30;
+scene.add( line1 );
+
 k = -500
 animate();
 
@@ -69,6 +102,43 @@ function animate() {
 
     requestAnimationFrame(animate);
 
+    if (k1 >= 500 & k1 < 1001) {
+        if (k1 % 100 === 0)
+        var geometry = new THREE.SphereGeometry(3 + ((k1 - 500) / 500 * 3), 4 + ((k1 - 500) / 100 * 2), 4 + ((k1 - 500) / 100 * 2));
+        var material = new THREE.MeshNormalMaterial();
+        if (k1 == 600) {
+            var sphere2 = new THREE.Mesh( geometry, material );
+            sphere2.position.z -= 30;
+            scene.add( sphere2 );
+            sphere1.radius -= 0.4;
+        }
+        if (k1 == 700) {
+            var sphere3 = new THREE.Mesh( geometry, material );
+            sphere3.position.z -= 30;
+            scene.add( sphere3 );
+            sphere1.geometry.radius -= 0.4;
+        }
+        if (k1 == 800) {
+            var sphere4 = new THREE.Mesh( geometry, material );
+            sphere4.position.z -= 30;
+            scene.add( sphere4 );
+            sphere1.geometry.radius -= 0.4;
+        }
+        if (k1 == 900) {
+            var sphere5 = new THREE.Mesh( geometry, material );
+            sphere5.position.z -= 30;
+            scene.add( sphere5 );
+            sphere1.geometry.radius -= 0.4;
+        }
+        if (k1 == 1000) {
+            var sphere6 = new THREE.Mesh( geometry, material );
+            sphere6.position.z -= 30;
+            scene.add( sphere6 );
+            sphere1.geometry.radius -= 0.4;
+        }
+        k1 += 1;
+    }
+
     playerControls();
     if (k < 240 & k > 0) {
         cube1.rotation.y += 0.01;
@@ -85,6 +155,28 @@ function animate() {
     if (k < 1) {
         k += 1;
     }
+    if (k1 < 500) {
+        sphere.rotation.x += 0.02;
+        sphere.rotation.z += 0.02;
+    }
+
+    line.rotation.y += 0.01;
+    line.rotation.z += 0.01;
+
+    line1.rotation.y += 0.03;
+    line1.rotation.z += 0.03;
+    
+
+    if (k == 240 & k1 < 500) {
+        k1 += 1;
+    }
+
+    if (k1 > 1000) {
+        line.visible = false;
+        line1.visible = false;
+    }
+
+    renderer.render(scene, camera);
 
     renderer.render(scene, camera);
 
